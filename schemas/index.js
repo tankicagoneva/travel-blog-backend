015806db5ac5bc54bf7d0512a491ffc28d9547cb
tankicagoneva <tankicaspasev@gmail.com>
2022-12-26 23:27:16 +0000

    travel blog backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import post from './post'
+import blockContent from './blockContent'
+import category from './category'
+import author from './author'
+
+export const schemaTypes = [post, blockContent, category, author]
